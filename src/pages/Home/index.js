b1f845fa2ecf287f9a9f035856042ba0c1bfabd4
Pablo Ykaro <pabloroox-zen@hotmail.com>
2022-05-30 23:44:03 -0300

    tonico-indiano

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,63 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 import { useNavigate } from 'react-router-dom';
 import "./style.css";
-function Home(){
-
+function Home({props,statusOne}){
+    const [ButtonO, setButtonO] = useState(false);
+    const [ButtonS, setButtonS] = useState(false);
 const navigate = useNavigate();
 
+
+const dados = [
+   ["Eliminar peso urgente?"],
+   ["Perder entre 10 a 30kg?"],
+];
+
 function MoveVideo(){
-    navigate("/video");
+    setTimeout(() => {
+        navigate("/video");
+    }, 100);
+    
 }
+function Dados(){
+    if(ButtonO===false && ButtonS===false){
+      return  dados[0];
+    }else if(ButtonS===true && ButtonO===true){
+       return dados[1]
+    }else{
+        return dados[1];
+    }
+}
+useEffect(()=>{
+   
+    if(ButtonS===true){
+        MoveVideo();
+    }
+},[ButtonS]);
+
 
  return (
      <div className="container">
         <div className="button-content">
-
             <div className="top-text">
-                TUDO PRONTO!!!
+                Tudo pronto!, o que você precisa ?
             </div>
             <div className="center-text">
-                CLIQUE NO BOTÃO PARA CONTINUAR
-                <button onClick={() => MoveVideo()} className="button-enter">CONTINUAR</button>
+             
+            <div className='header-post'>{Dados()}</div>
+           
+               {ButtonO===false ? 
+                <>
+                <button onClick={() => setButtonO(true)}  className="button-enter">SIM</button>
+                <button onClick={() => setButtonO(true)}  className="button-enter-red">NÃO</button>
+                </> :
+                 <>
+                <button onClick={() => setButtonS(true)}  className="button-enter">SIM</button>
+                <button onClick={() => setButtonS(true)}  className="button-enter-red">NÃO</button>
+                </>
+                }
+           
+
+       
 
             </div>
         
@@ -27,4 +66,7 @@ function MoveVideo(){
      </div>
  );
 }
+
+
+
 export default Home;
